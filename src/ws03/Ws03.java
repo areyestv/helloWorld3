9f348fa4490b06d8af030c6eee9b4ed977a1f0bf
alain <areyes@televida.biz>
2018-02-15 11:29:25 -0600

    test

--- src/ws03/Ws03.java
+++ src/ws03/Ws03.java
@@ -19,7 +19,7 @@ public class Ws03 extends HttpServlet {
      */
     public Ws03() {
         super();
-        // TODO Auto-generated constructor stub
+        // TODO Auto-generated constructor stub 
     }
 
 	/**
@@ -27,7 +27,7 @@ public class Ws03 extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Served at: xyz ").append(request.getContextPath());
 	}
 
 	/**
